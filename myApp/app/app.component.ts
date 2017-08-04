e7351f1fec7073dfda61181fc4263bef134f2fd4
ramesh <ramesh786mullapudi@gmail.com>
2017-08-04 16:32:38 -0500

    Added Router

--- myApp/app/app.component.ts
+++ myApp/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { ProductService } from './products/product.service';
 @Component ({
 selector : 'sdp-app',
 template:`
-         <h1>{{pageTitle}}</h1>
-         <sdp-products></sdp-products>
-         `
+<div>
+<nav class='navbar navbar-default'>
+  <div class='container-fluid'>
+      <a class='navbar-brand'>{{pageTitle}}</a>
+      <ul class='nav navbar-nav'>
+        <li><a [routerLink]="['/welcome']">Home</a></li>
+        <li><a [routerLink]="['/products']">Product List</a></li>
+      </ul>
+  </div>
+</nav>
+<div class='container'>
+            <router-outlet></router-outlet>
+</div>
+</div>
+         `,
+providers: [ProductService]
 })
 export class AppComponent {
     pageTitle:string = "Shop Disney Parks";
--- myApp/app/products/product-list.component.ts
+++ myApp/app/products/product-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import  { IProduct } from './product';
+import { ProductService } from './product.service';
 @Component({
-  selector: 'sdp-products',
   moduleId:module.id,
   templateUrl:'product-list.component.html',
   styleUrls: ['product-list.component.css']
@@ -11,65 +11,20 @@ export class ProductListComponent implements OnInit {
    imageWidth:number = 100;
    imageMargin:number = 2;
    showImage:boolean = false;
-   listFilter:string = 'cart';
-   products:IProduct[]= [
-     {
-         "productId": 1,
-         "productName": "Leaf Rake",
-         "productCode": "GDN-0011",
-         "releaseDate": "March 19, 2016",
-         "description": "Leaf rake with 48-inch wooden handle.",
-         "price": 19.95,
-         "starRating": 3.2,
-         "imageUrl": "http://www.diszine.com/wp-content/uploads/2010/06/Dismerch-300x225.jpg"
-     },
-     {
-         "productId": 2,
-         "productName": "Garden Cart",
-         "productCode": "GDN-0023",
-         "releaseDate": "March 18, 2016",
-         "description": "15 gallon capacity rolling garden cart",
-         "price": 32.99,
-         "starRating": 4.2,
-         "imageUrl": "http://www.disneygeekery.com/wp-content/uploads/2015/06/VANS-X-DISNEY_ADULT_FOOTWEAR_PACK_banner-560x280.jpg"
-     },
-     {
-         "productId": 5,
-         "productName": "Hammer",
-         "productCode": "TBX-0048",
-         "releaseDate": "May 21, 2016",
-         "description": "Curved claw steel hammer",
-         "price": 8.9,
-         "starRating": 4.8,
-         "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-     },
-     {
-         "productId": 8,
-         "productName": "Saw",
-         "productCode": "TBX-0022",
-         "releaseDate": "May 15, 2016",
-         "description": "15-inch steel blade hand saw",
-         "price": 11.55,
-         "starRating": 3.7,
-         "imageUrl": "http://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
-     },
-     {
-         "productId": 10,
-         "productName": "Video Game Controller",
-         "productCode": "GMG-0042",
-         "releaseDate": "October 15, 2015",
-         "description": "Standard two-button video game controller",
-         "price": 35.95,
-         "starRating": 4.6,
-         "imageUrl": "http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
-     }
-   ];
+   listFilter:string;
+   errorMessage:string;
+   products:IProduct[];
+
+   constructor (private _productService:ProductService) {
+   };
  toggleImage ():void {
    this.showImage = !this.showImage;
  };
 
  ngOnInit():void {
-   console.log('OnInit');
+   this._productService.getProducts()
+   .subscribe(ram => this.products= ram,
+   error => this.errorMessage = <any>error );
  };
 
  onRatingClicked(message:string):void{
